274c08f8ab133175da7921f458a27fab4408cf32
Sai Prakash Gundemeda <113116997+Saiprakashgundemeda@users.noreply.github.com>
2024-01-23 13:06:23 +0530

    Update index.js

--- src/components/EvenOddApp/index.js
+++ src/components/EvenOddApp/index.js
@@ -4,34 +4,31 @@ import './index.css'
 
 class EvenOddApp extends Component {
   state = {
-    increaseCount: 0,
+    onIncreaseRandomNumber: 0,
   }
 
   onRandomNumber = () => Math.ceil(Math.random() * 100)
 
-  checkEvenOdd = () => {
-    console.log('clicked')
-    const randomNumber = this.onRandomNumber()
-    this.setState(prevState => ({
-      increaseCount: prevState.increaseCount + randomNumber,
-    }))
+  onIncrement = () => {
+    const getRandomNumber = this.onRandomNumber()
+    this.setState({
+      onIncreaseRandomNumber: getRandomNumber,
+    })
   }
 
   render() {
-    const {increaseCount} = this.state
-    const evenOddText = increaseCount % 2 === 0 ? 'Even' : 'Odd'
+    const {onIncreaseRandomNumber} = this.state
+    const EvenOddText = onIncreaseRandomNumber % 2 === 0 ? 'Even' : 'Odd'
 
     return (
-      <div className="bgSection">
-        <div className="bgSection2">
-          <h1 className="heading">Count {increaseCount}</h1>
-          <p className="paragraph">Count is {evenOddText}</p>
-          <button type="button" className="button" onClick={this.checkEvenOdd}>
-            On Increment
+      <div className="background-container">
+        <div className="card-container">
+          <h1>Count {onIncreaseRandomNumber}</h1>
+          <p className="p1">Count is {EvenOddText} </p>
+          <button type="button" className="button" onClick={this.onIncrement}>
+            Increment
           </button>
-          <p className="paragraph2">
-            *Increase By Random Number Between 0 to 100
-          </p>
+          <p className="p2">*Increase By Random Number Between 0 to 100</p>
         </div>
       </div>
     )
